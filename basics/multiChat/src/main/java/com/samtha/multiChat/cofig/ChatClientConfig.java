cbf4a5f956e262445b74f375908240e9024959ab
manidhar <manidhar.karnatakam@gmail.com>
2025-12-03 07:57:31 +0530

    Multi Model Example

---
+++ basics/multiChat/src/main/java/com/samtha/multiChat/cofig/ChatClientConfig.java
@@ -0,0 +1,24 @@
+package com.samtha.multiChat.cofig;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient openAiChatClient(OpenAiChatModel openAiChatModel) {
+        return ChatClient.create(openAiChatModel);
+    }
+
+    // This approach of creating the object has more control when compared to the above one.
+    @Bean
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        ChatClient.Builder chatClientBuilder = ChatClient.builder(ollamaChatModel);
+        return chatClientBuilder.build();
+    }
+
+}
---
+++ basics/multiChat/src/test/java/com/samtha/multiChat/MultiChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samtha.multiChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
