f080a2a372bce0bff70628e7de893795bbb81dbb
manidhar <manidhar.karnatakam@gmail.com>
2025-12-03 07:55:10 +0530

    basics
    llm using ollama

---
+++ basics/ollamaai/src/test/java/com/samtha/ollamaai/OllamaaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samtha.ollamaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OllamaaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
